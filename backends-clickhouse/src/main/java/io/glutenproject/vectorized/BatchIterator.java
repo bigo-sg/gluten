915cdcbde3c5c717b424e3f5ebb53467b234cdd7
taiyang-li <654010905@qq.com>
2022-10-09 20:19:55 +0800

    replace tab with 2 blanks

--- backends-clickhouse/src/main/java/io/glutenproject/vectorized/BatchIterator.java
+++ backends-clickhouse/src/main/java/io/glutenproject/vectorized/BatchIterator.java
@@ -26,45 +26,45 @@ import java.io.IOException;
 import java.util.List;
 
 public class BatchIterator extends GeneralOutIterator {
-	public BatchIterator(long instance_id, List<Attribute> outAttrs) throws IOException {
-		super(instance_id, outAttrs);
-	}
+  public BatchIterator(long instance_id, List<Attribute> outAttrs) throws IOException {
+    super(instance_id, outAttrs);
+  }
 
-	private native boolean nativeHasNext(long nativeHandle);
+  private native boolean nativeHasNext(long nativeHandle);
 
-	private native byte[] nativeNext(long nativeHandle);
+  private native byte[] nativeNext(long nativeHandle);
 
-	private native long nativeCHNext(long nativeHandle);
+  private native long nativeCHNext(long nativeHandle);
 
-	private native void nativeClose(long nativeHandle);
+  private native void nativeClose(long nativeHandle);
 
-	private native Metrics nativeFetchMetrics(long nativeHandle);
+  private native Metrics nativeFetchMetrics(long nativeHandle);
 
-	@Override
-	public boolean hasNextInternal() throws IOException {
-		return nativeHasNext(handle);
-	}
+  @Override
+  public boolean hasNextInternal() throws IOException {
+    return nativeHasNext(handle);
+  }
 
-	@Override
-	public ColumnarBatch nextInternal() throws IOException {
-		long block = nativeCHNext(handle);
-		CHNativeBlock nativeBlock = new CHNativeBlock(block);
-		int cols = nativeBlock.numColumns();
-		ColumnVector[] columnVectors = new ColumnVector[cols];
-		for (int i = 0; i < cols; i++) {
-			columnVectors[i] = new CHColumnVector(CHExecUtil.inferSparkDataType(nativeBlock.getTypeByPosition(i)),
-					block, i);
-		}
-		return new ColumnarBatch(columnVectors, nativeBlock.numRows());
-	}
+  @Override
+  public ColumnarBatch nextInternal() throws IOException {
+    long block = nativeCHNext(handle);
+    CHNativeBlock nativeBlock = new CHNativeBlock(block);
+    int cols = nativeBlock.numColumns();
+    ColumnVector[] columnVectors = new ColumnVector[cols];
+    for (int i = 0; i < cols; i++) {
+      columnVectors[i] = new CHColumnVector(CHExecUtil.inferSparkDataType(nativeBlock.getTypeByPosition(i)),
+          block, i);
+    }
+    return new ColumnarBatch(columnVectors, nativeBlock.numRows());
+  }
 
-	@Override
-	public Metrics getMetricsInternal() throws IOException, ClassNotFoundException {
-		return nativeFetchMetrics(handle);
-	}
+  @Override
+  public Metrics getMetricsInternal() throws IOException, ClassNotFoundException {
+    return nativeFetchMetrics(handle);
+  }
 
-	@Override
-	public void closeInternal() {
-		nativeClose(handle);
-	}
+  @Override
+  public void closeInternal() {
+    nativeClose(handle);
+  }
 }
