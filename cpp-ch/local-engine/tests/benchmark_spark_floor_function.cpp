c34c504d08fbae9a669595b336477acb3c3c0231
taiyang-li <654010905@qq.com>
2024-01-24 11:42:34 +0800

    poc to improve if when result type is floating point or small decimal

--- cpp-ch/local-engine/tests/benchmark_spark_floor_function.cpp
+++ cpp-ch/local-engine/tests/benchmark_spark_floor_function.cpp
@@ -222,8 +222,234 @@ static void BMNanInfToNull(benchmark::State & state)
 }
 BENCHMARK(BMNanInfToNull);
 
-
 BENCHMARK(BM_CHFloorFunction_For_Int64);
 BENCHMARK(BM_CHFloorFunction_For_Float64);
 BENCHMARK(BM_SparkFloorFunction_For_Int64);
 BENCHMARK(BM_SparkFloorFunction_For_Float64);
+
+
+/*
+/// TO run in https://quick-bench.com/q/h-2qGgqxM8ksp57VD0w7JdKKN-I
+using UInt8 = unsigned char;
+using UInt64 = unsigned long long;
+using Int64 = signed long long;
+template<typename T>
+using PaddedPODArray = std::vector<T>;
+*/
+
+/*
+Benchmark when BranchType is Float64
+---------------------------------------------------------------
+Benchmark                     Time             CPU   Iterations
+---------------------------------------------------------------
+BM_fillVectorVector1     414177 ns       414161 ns         1687
+BM_fillVectorVector2      96669 ns        96665 ns         7432
+BM_fillVectorVector3      78439 ns        78436 ns         8812
+
+Benchmark when BranchType is Int64
+---------------------------------------------------------------
+Benchmark                     Time             CPU   Iterations
+---------------------------------------------------------------
+BM_fillVectorVector1      80645 ns        80643 ns         8101
+BM_fillVectorVector2      73841 ns        73838 ns         9484
+BM_fillVectorVector3      73883 ns        73881 ns         9485
+
+Benchmark when BranchType is Decimal64
+---------------------------------------------------------------
+Benchmark                     Time             CPU   Iterations
+---------------------------------------------------------------
+BM_fillVectorVector1      82413 ns        82408 ns         8635
+BM_fillVectorVector2      76289 ns        76287 ns         9213
+BM_fillVectorVector3      76262 ns        76260 ns         9244
+
+Benchmark when BranchType is Int256
+---------------------------------------------------------------
+Benchmark                     Time             CPU   Iterations
+---------------------------------------------------------------
+BM_fillVectorVector1     307741 ns       307726 ns         2263
+BM_fillVectorVector2    2184999 ns      2184903 ns          321
+BM_fillVectorVector3     318616 ns       318605 ns         2209
+
+Benchmark when BranchType is Decimal256
+---------------------------------------------------------------
+Benchmark                     Time             CPU   Iterations
+---------------------------------------------------------------
+BM_fillVectorVector1     303179 ns       303164 ns         2311
+BM_fillVectorVector3     305023 ns       305010 ns         2266
+*/
+
+/*
+Some commands that would be helpful
+
+# run benchmark
+./build_gcc/utils/extern-local-engine/tests/benchmark_local_engine --benchmark_filter="BM_fillVectorVector*"
+
+# get full symbol name
+objdump -t  ./build_gcc/utils/extern-local-engine/tests/benchmark_local_engine   |  c++filt   | grep "fillVectorVector1"
+
+# get assembly code mixed with source code by symbol name
+gdb -batch -ex "disassemble/rs 'void fillVectorVector3<double, double>(DB::PODArray<char8_t, 4096ul, Allocator<false, false>, 63ul, 64ul> const&, DB::PODArray<double, 4096ul, Allocator<false, false>, 63ul, 64ul> const&, DB::PODArray<double, 4096ul, Allocator<false, false>, 63ul, 64ul> const&, DB::PODArray<double, 4096ul, Allocator<false, false>, 63ul, 64ul>&)'" ./build_gcc/utils/extern-local-engine/tests/benchmark_local_engine    | c++filt  > 3.S
+*/
+
+using ResultType = Decimal64;
+
+template <typename Branch1Type = ResultType, typename Branch2Type = ResultType>
+static NO_INLINE void fillVectorVector0(const PaddedPODArray<UInt8> & cond, Branch1Type a, Branch2Type b, PaddedPODArray<ResultType> & res)
+{
+    size_t rows = cond.size();
+    for (size_t i = 0; i < rows; ++i)
+    {
+        res[i] = cond[i] ? static_cast<ResultType>(a) : static_cast<ResultType>(b);
+    }
+}
+
+template <typename Branch1Type = ResultType, typename Branch2Type = ResultType>
+static NO_INLINE void fillVectorVector1(
+    const PaddedPODArray<UInt8> & cond,
+    const PaddedPODArray<Branch1Type> & a,
+    const PaddedPODArray<Branch2Type> & b,
+    PaddedPODArray<ResultType> & res)
+{
+    size_t rows = cond.size();
+    for (size_t i = 0; i < rows; ++i)
+    {
+        res[i] = cond[i] ? static_cast<ResultType>(a[i]) : static_cast<ResultType>(b[i]);
+    }
+}
+
+template <typename Branch1Type = ResultType, typename Branch2Type = ResultType>
+static NO_INLINE void fillVectorVector2(
+    const PaddedPODArray<UInt8> & cond,
+    const PaddedPODArray<Branch1Type> & a,
+    const PaddedPODArray<Branch2Type> & b,
+    PaddedPODArray<ResultType> & res)
+{
+    size_t rows = cond.size();
+    for (size_t i = 0; i < rows; ++i)
+    {
+        res[i] = (!!cond[i]) * static_cast<ResultType>(a[i]) + (!cond[i]) * static_cast<ResultType>(b[i]);
+    }
+}
+
+template <typename Branch1Type = ResultType, typename Branch2Type = ResultType>
+static NO_INLINE void fillVectorVector3(
+    const PaddedPODArray<UInt8> & cond,
+    const PaddedPODArray<Branch1Type> & a,
+    const PaddedPODArray<Branch2Type> & b,
+    PaddedPODArray<ResultType> & res)
+{
+    size_t rows = cond.size();
+    for (size_t i = 0; i < rows; ++i)
+    {
+        if constexpr (std::is_integral_v<ResultType> || (is_decimal<ResultType> && sizeof(ResultType) <= 8))
+        {
+            res[i] = (!!cond[i]) * static_cast<ResultType>(a[i]) + (!cond[i]) * static_cast<ResultType>(b[i]);
+        }
+        else if constexpr (std::is_floating_point_v<ResultType>)
+        {
+            using UIntType = std::conditional_t<sizeof(ResultType) == 8, UInt64, UInt32>;
+            using IntType = std::conditional_t<sizeof(ResultType) == 8, Int64, Int32>;
+            auto mask = static_cast<UIntType>(static_cast<IntType>(cond[i]) - 1);
+            auto new_a = static_cast<ResultType>(a[i]);
+            auto new_b = static_cast<ResultType>(b[i]);
+            auto tmp = (~mask & (*reinterpret_cast<const UIntType *>(&new_a))) | (mask & (*reinterpret_cast<const UIntType *>(&new_b)));
+            res[i] = *(reinterpret_cast<ResultType *>(&tmp));
+        }
+        else
+        {
+            res[i] = cond[i] ? static_cast<ResultType>(a[i]) : static_cast<ResultType>(b[i]);
+        }
+    }
+}
+
+static constexpr size_t ROWS = 65536;
+static void initCondition(PaddedPODArray<UInt8> & cond)
+{
+    cond.resize(ROWS);
+    for (size_t i = 0; i < ROWS; ++i)
+    {
+        cond[i] = std::rand() % 2;
+    }
+}
+
+template <typename T>
+static void initBranch(PaddedPODArray<T> & branch)
+{
+    branch.resize(ROWS);
+    for (size_t i = 0; i < ROWS; ++i)
+    {
+        branch[i] = static_cast<T>(std::rand());
+    }
+}
+
+
+static void BM_fillVectorVector0(benchmark::State & state)
+{
+    PaddedPODArray<UInt8> cond;
+    ResultType a = std::rand();
+    ResultType b = std::rand();
+    PaddedPODArray<ResultType> res(ROWS);
+    initCondition(cond);
+
+    for (auto _ : state)
+    {
+        fillVectorVector0(cond, a, b, res);
+        benchmark::DoNotOptimize(res);
+    }
+}
+
+static void BM_fillVectorVector1(benchmark::State & state)
+{
+    PaddedPODArray<UInt8> cond;
+    PaddedPODArray<ResultType> a;
+    PaddedPODArray<ResultType> b;
+    PaddedPODArray<ResultType> res(ROWS);
+    initCondition(cond);
+    initBranch(a);
+    initBranch(b);
+
+    for (auto _ : state)
+    {
+        fillVectorVector1(cond, a, b, res);
+        benchmark::DoNotOptimize(res);
+    }
+}
+
+static void BM_fillVectorVector2(benchmark::State & state)
+{
+    PaddedPODArray<UInt8> cond;
+    PaddedPODArray<ResultType> a;
+    PaddedPODArray<ResultType> b;
+    PaddedPODArray<ResultType> res(ROWS);
+    initCondition(cond);
+    initBranch(a);
+    initBranch(b);
+
+    for (auto _ : state)
+    {
+        fillVectorVector2(cond, a, b, res);
+        benchmark::DoNotOptimize(res);
+    }
+}
+
+static void BM_fillVectorVector3(benchmark::State & state)
+{
+    PaddedPODArray<UInt8> cond;
+    PaddedPODArray<ResultType> a;
+    PaddedPODArray<ResultType> b;
+    PaddedPODArray<ResultType> res(ROWS);
+    initCondition(cond);
+    initBranch(a);
+    initBranch(b);
+
+    for (auto _ : state)
+    {
+        fillVectorVector3(cond, a, b, res);
+        benchmark::DoNotOptimize(res);
+    }
+}
+
+BENCHMARK(BM_fillVectorVector0);
+BENCHMARK(BM_fillVectorVector1);
+BENCHMARK(BM_fillVectorVector2);
+BENCHMARK(BM_fillVectorVector3);
